65e8737003ebaa8e9fcf64fbfe03c53408d62da4
Davide <bubini.mara5@gmail.com>
2018-04-24 17:57:17 +0200

    spoon integration with gradle task

--- app/src/androidTest/java/com/github/bubinimara/movies/app/ui/activity/main/MainActivityTest.java
+++ app/src/androidTest/java/com/github/bubinimara/movies/app/ui/activity/main/MainActivityTest.java
@@ -1,10 +1,12 @@
 package com.github.bubinimara.movies.app.ui.activity.main;
 
+import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.github.bubinimara.movies.R;
 import com.github.bubinimara.movies.app.rules.EmptyDataModuleRule;
+import com.squareup.spoon.Spoon;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -24,6 +26,7 @@ import static org.hamcrest.core.AllOf.allOf;
 /**
  * Created by davide.
  */
+@SmallTest
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
@@ -41,18 +44,22 @@ public class MainActivityTest {
 
     @Test
     public void bottom_navigation(){
-
+        Spoon.screenshot(mActivityRule.getActivity(), "initial_state");
         onView(withId(R.id.viewPager)).check(matches(isDisplayed()));
         onView(withId(R.id.navigation)).check(matches(isCompletelyDisplayed()));
 
+
         onView(withId(R.id.navigation_home)).perform(click());
         onView(allOf(withId(R.id.home_title),withText(R.string.home_most_popular))).check(matches(isDisplayed()));
+        Spoon.screenshot(mActivityRule.getActivity(), "home_screen");
 
         onView(withId(R.id.navigation_profile)).perform(click());
         onView(allOf(withId(R.id.btn_login),withText(R.string.btn_login))).check(matches(isDisplayed()));
+        Spoon.screenshot(mActivityRule.getActivity(), "profile_screen");
 
         onView(withId(R.id.navigation_search)).perform(click());
         onView(allOf(withId(R.id.search_text),withHint(R.string.hint_search))).check(matches(isCompletelyDisplayed()));
+        Spoon.screenshot(mActivityRule.getActivity(), "search_screen");
 
     }
 }
