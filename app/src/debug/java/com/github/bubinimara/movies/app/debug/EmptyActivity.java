ba8c4c3d32a7b4e401dc7b31bb60ca5901dfcfe3
Davide <bubini.mara5@gmail.com>
2019-04-05 15:15:32 +0200

    fix error in instrumentation test

--- app/src/debug/java/com/github/bubinimara/movies/app/debug/EmptyActivity.java
+++ app/src/debug/java/com/github/bubinimara/movies/app/debug/EmptyActivity.java
@@ -2,6 +2,7 @@ package com.github.bubinimara.movies.app.debug;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,10 @@ import android.widget.RelativeLayout;
  */
 public class EmptyActivity extends AppCompatActivity {
 
+    private static final String VIEW_ROOT_ID = "ARGS_VIEW_ROOT_ID";
     private ViewGroup content;
+
+    private int viewRootId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,24 @@ public class EmptyActivity extends AppCompatActivity {
         relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         content = new FrameLayout(this);
         content.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        content.setId(getContentId(savedInstanceState));
         relativeLayout.addView(content);
         setContentView(relativeLayout);
+    }
+
+    private int getContentId(Bundle savedInstanceState) {
+        if(savedInstanceState == null){
+            viewRootId = ViewCompat.generateViewId();
+        }else{
+            viewRootId = savedInstanceState.getInt(VIEW_ROOT_ID);
+        }
+        return viewRootId;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(VIEW_ROOT_ID,viewRootId);
+        super.onSaveInstanceState(outState);
     }
 
     public void addContent(View view){
